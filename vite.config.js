c7b2c21e556c103b13e0d8bba0911c7738af1212
LinuxEclipsed <kalbert1991@gmail.com>
2025-06-09 15:51:23 -0700

    allow api calls without ingress

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: true
+    allowedHosts: true,
+    proxy: {
+      '/api': {
+        target: 'http://waittimes-api:8000',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, ''),
+      }
+    }
   }
 })
